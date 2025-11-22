27c897e26690b135ea8d62acd465212ff8651afc
mhmddefrizal <muhammaddefrizal08@gmail.com>
2025-11-22 23:32:04 +0700

    tambah file baru navlink.tsx

---
+++ web/components/navbar/navlink.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navlink = () => {
+  return (
+    <>
+      <button className="inline-flex items-center p-2 justify-center text-sm text-gray-500 rounded-md md:hidden hover:bg-gray-100"></button>
+    </>
+  );
+};
+
+export default Navlink;
