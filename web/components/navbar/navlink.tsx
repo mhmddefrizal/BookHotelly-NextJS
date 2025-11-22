06f2496597fd8bc25724c40239db60c91508b0d4
mhmddefrizal <muhammaddefrizal08@gmail.com>
2025-11-22 23:42:30 +0700

    buat use state awal di navlink.tsx

--- web/components/navbar/navlink.tsx
+++ web/components/navbar/navlink.tsx
@@ -1,6 +1,9 @@
+"use client";
+import { useState } from "react";
 import { IoClose, IoMenu } from "react-icons/io5";
 
 const Navlink = () => {
+  const [open, setOpen] = useState(false);
   return (
     <>
       <button className="inline-flex items-center p-2 justify-center text-sm text-gray-500 rounded-md md:hidden hover:bg-gray-100"></button>
