73726f25300ed9adbd24952e8bf7e0184f33e479
mhmddefrizal <muhammaddefrizal08@gmail.com>
2025-11-23 00:02:10 +0700

    hubungkan state open dengan tampilan menu

--- web/components/navbar/navlink.tsx
+++ web/components/navbar/navlink.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import { IoClose, IoMenu } from "react-icons/io5";
+import clsx from "clsx";
 
 const Navlink = () => {
   const [open, setOpen] = useState(false);
@@ -9,6 +10,14 @@ const Navlink = () => {
       <button onClick={() => setOpen(!open)} className="inline-flex items-center p-2 justify-center text-sm text-gray-500 rounded-md md:hidden hover:bg-gray-100">
         {!open ? <IoMenu className="size-8" /> : <IoClose className="size-8" />}
       </button>
+
+      <div
+        className={clsx("w-full md:block md:w-auto", {
+          hidden: !open,
+        })}
+      >
+        <ul className="flex flex-col font-semibold text-sm uppercase p-4 mt-4 rounded-sm bg-gray-50 md:flex-row md:items-center md:space-x-10"></ul>
+      </div>
     </>
   );
 };
