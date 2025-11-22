6bbd44c421dfddfdea51abe2d3bebd2b0eed754a
mhmddefrizal <muhammaddefrizal08@gmail.com>
2025-11-22 22:49:54 +0700

    langkah awal buat navbar

---
+++ web/components/navbar/navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="fixed top-0 w-full bg-white shadow-sm z-20">
+      <div className="max-w-screen-xl mx-auto flex flex-wrap items-center justify-between p-4"></div>
+    </div>
+  );
+};
+
+export default Navbar;
