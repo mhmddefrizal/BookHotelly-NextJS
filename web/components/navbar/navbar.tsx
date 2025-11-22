4fd1b4ba15da1d0087951a933def77a952e1301c
mhmddefrizal <muhammaddefrizal08@gmail.com>
2025-11-22 23:20:06 +0700

    import logo

--- web/components/navbar/navbar.tsx
+++ web/components/navbar/navbar.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <div className="fixed top-0 w-full bg-white shadow-sm z-20">
-      <div className="max-w-screen-xl mx-auto flex flex-wrap items-center justify-between p-4"></div>
+      <div className="max-w-screen-xl mx-auto flex flex-wrap items-center justify-between p-4">
+        <Link href="/">
+          <Image src="/logo.png" width={128} height={49} alt="logo" priority />
+        </Link>
+      </div>
     </div>
   );
 };
