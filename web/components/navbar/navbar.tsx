57409a04b856d196aa97e1e685f2a6e7716fdc8d
mhmddefrizal <muhammaddefrizal08@gmail.com>
2025-11-23 16:07:42 +0700

    import navlink di navbar

--- web/components/navbar/navbar.tsx
+++ web/components/navbar/navbar.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-
+import Navlink from "@/components/navbar/navlink";
 const Navbar = () => {
   return (
     <div className="fixed top-0 w-full bg-white shadow-sm z-20">
@@ -8,6 +8,7 @@ const Navbar = () => {
         <Link href="/">
           <Image src="/logo.png" width={128} height={49} alt="logo" priority />
         </Link>
+        <Navlink />
       </div>
     </div>
   );
