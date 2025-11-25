58fb0549c2ecec205bf64897aae3a5ad8970bf59
okawindanasaputra <okwan2004@gmail.com>
2025-11-25 14:43:19 +0700

    menambahkan mb-10 dan display block

--- web/components/footer.tsx
+++ web/components/footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <div className="maxwith-screen-xl mx-auto px-4 w-full py-10 md:py-16">
             <div className="grid md:grid-cols-3 gap-7">
                 <div>
-                    <Link href="/">
+                    <Link href="/" className="mb-10 block">
                         <Image src="/logo.png" width={96} height={36} alt="logo"/>
                     </Link>
                 </div>
