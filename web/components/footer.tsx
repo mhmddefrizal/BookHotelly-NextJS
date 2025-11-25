973ceefca1897db69a9d97cc3716c800f851a0f6
okawindanasaputra <okwan2004@gmail.com>
2025-11-25 14:33:24 +0700

    menambahkan layout grid 3 kolom

--- web/components/footer.tsx
+++ web/components/footer.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className="bg-gray-900">
-        <div className="maxwith-screen-xl mx-auto px-4 w-full py-10 md:py-16"></div>
+        <div className="maxwith-screen-xl mx-auto px-4 w-full py-10 md:py-16">
+            <div className="grid md:grid-cols-3 gap-7"></div>
+        </div>
         <div className="maxwith-screen-xl mx-auto px-4 border-t border-gray-800 py-8 txet-center
         text-base text-gray-600">
             &copy; Copyright 2025 | Limo Nago |
