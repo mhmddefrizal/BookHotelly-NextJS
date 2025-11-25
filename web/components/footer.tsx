db8640eedb7011fe12fba88e18280466f1668475
okawindanasaputra <okwan2004@gmail.com>
2025-11-25 14:35:10 +0700

    menambahkan tiga div

--- web/components/footer.tsx
+++ web/components/footer.tsx
@@ -4,7 +4,11 @@ const Footer = () => {
   return (
     <footer className="bg-gray-900">
         <div className="maxwith-screen-xl mx-auto px-4 w-full py-10 md:py-16">
-            <div className="grid md:grid-cols-3 gap-7"></div>
+            <div className="grid md:grid-cols-3 gap-7">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
         </div>
         <div className="maxwith-screen-xl mx-auto px-4 border-t border-gray-800 py-8 txet-center
         text-base text-gray-600">
