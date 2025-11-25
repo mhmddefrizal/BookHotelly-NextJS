e5225b37bbac312c5983fdc3f7b91cc8ed538c3f
okawindanasaputra <okwan2004@gmail.com>
2025-11-25 14:23:40 +0700

    tyle: menambahkan layout dan text style pada div

--- web/components/footer.tsx
+++ web/components/footer.tsx
@@ -3,6 +3,11 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className="bg-gray-900">
+        <div></div>
+        <div className="maxwith-screen-xl mx-auto px-4 border-t border-gray-800 py-8 txet-center
+        text-base text-gray-600">
+            
+        </div>
     </footer>
   )
 }
