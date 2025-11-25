bb552d122a0f382a4392e802921e71f46c5efafe
okawindanasaputra <okwan2004@gmail.com>
2025-11-25 14:31:26 +0700

    menambahkan layout container

--- web/components/footer.tsx
+++ web/components/footer.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className="bg-gray-900">
-        <div></div>
+        <div className="maxwith-screen-xl mx-auto px-4 w-full py-10 md:py-16"></div>
         <div className="maxwith-screen-xl mx-auto px-4 border-t border-gray-800 py-8 txet-center
         text-base text-gray-600">
-            &copy; Copyright 2025 | Limo Nago | All Rights Reserved
+            &copy; Copyright 2025 | Limo Nago |
         </div>
     </footer>
   )
