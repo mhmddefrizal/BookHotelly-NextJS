e7421754e53f49f08787465eb5a32eab486f9da9
Oka Windana Saputra <okwan2004@gmail.com>
2025-11-28 16:00:34 +0700

    menambahkan styling object-cover dan rounded-t-sm pada image

--- web/components/card.tsx
+++ web/components/card.tsx
@@ -4,7 +4,8 @@ function Card() {
   return (
     <div className="bg-white shadow-lg rounded-sm transition duration-100 hover:shadow-sm">
       <div className="h-[260px] w-auto rounded-t-sm relative">
-        <Image src="/hero.jpg" width={345} height={240} alt="room image" />
+        <Image src="/hero.jpg" width={345} height={240} alt="room image" className="w-full h-full
+        object-cover rounded-t-sm"/>
       </div>
     </div>
   );
