04278b99fafe6fcd1b81bbd7b931216dcc650fa7
Rivaldzz <rivaldosborn@gmail.com>
2025-11-28 19:42:44 +0700

    Update 1 bagian tentang kami

--- web/app/about/page.tsx
+++ web/app/about/page.tsx
@@ -4,7 +4,9 @@ const AboutPage = () => {
   return (
     <div>
       <HeaderSection title="Tentang Kami" subTitle="Belum update" />
-      <div className="max-w-7xl mx-auto py-20 px-4"></div>
+      <div className="max-w-7xl mx-auto py-20 px-4">
+        <div className="grid md:grid-cols-2 gap-8"></div>
+      </div>
     </div>
   )
 }
