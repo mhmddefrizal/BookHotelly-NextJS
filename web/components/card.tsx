ffc716a266818c81e4c60f4cf5feaa7fef55a036
Oka Windana Saputra <okwan2004@gmail.com>
2025-11-28 15:48:31 +0700

    import Next.js Image component

--- web/components/card.tsx
+++ web/components/card.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import Image from "next/image"
 
 function Card() {
   return (
     <div className="bg-white shadow-lg rounded-sm transition duration-100 hover:shadow-sm">
-    <div className="h-[260px] w-auto rounded-t-sm relative"></div>
+    <div className="h-[260px] w-auto rounded-t-sm rela">
+        
+    </div>
     </div>
   )
 }
