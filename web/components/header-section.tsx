d47f996a55cfcbf2cfdba6f8ff3a3bdc63dd7176
Rivaldzz <rivaldosborn@gmail.com>
2025-11-28 19:34:45 +0700

    Update subtitle

--- web/components/header-section.tsx
+++ web/components/header-section.tsx
@@ -16,6 +16,7 @@ const HeaderSection = ({
         </div>
         <div className="relative flex flex-col justify-center items-center h-60  text-center pt-14">
             <h1 className='text-5xl font-bold leading-tight capitalize'>{title}</h1>
+            <p className='text-xl text-gray-300'>{subTitle}</p>
         </div>
 
     </header>
