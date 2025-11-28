4014e60bd5c6374392a521818373ff84581fa254
Rivaldzz <rivaldosborn@gmail.com>
2025-11-28 19:05:59 +0700

    Penambahan bagian main

---
+++ web/components/main.tsx
@@ -0,0 +1,19 @@
+// import React from 'react'
+import Card from "@/components/card";
+
+const main = () => {
+  return (
+    <div className="max-w-screen-xl py-6 pb-20 px-4 mx-auto ">
+        <div className="grip gap-7 md:grid-cols-3">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+        </div>
+    </div>
+  )
+}
+
+export default main
