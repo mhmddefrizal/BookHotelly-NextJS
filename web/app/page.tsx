20c6e60c452c999d4bd30872258c47fdf9e6cea0
Rivaldzz <rivaldosborn@gmail.com>
2025-11-28 19:10:29 +0700

    Update bagian page.tsx

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,9 +1,15 @@
 import Hero from "@/components/hero";
+import Main from "@/components/main";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <div className="mt-16">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold uppercase">Kamar dan Tarif</h1>
+        </div>
+      </div>
     </div>
   );
 }
