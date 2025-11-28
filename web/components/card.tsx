ddaa3e34840be579f009b9ca17c4583414c00b82
Oka Windana Saputra <okwan2004@gmail.com>
2025-11-28 15:58:26 +0700

    menambahkan gambar room menggunakan Next.js Image

--- web/components/card.tsx
+++ web/components/card.tsx
@@ -1,13 +1,13 @@
-import Image from "next/image"
+import Image from "next/image";
 
 function Card() {
   return (
     <div className="bg-white shadow-lg rounded-sm transition duration-100 hover:shadow-sm">
-    <div className="h-[260px] w-auto rounded-t-sm rela">
-        
+      <div className="h-[260px] w-auto rounded-t-sm relative">
+        <Image src="/hero.jpg" width={345} height={240} alt="room image" />
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
