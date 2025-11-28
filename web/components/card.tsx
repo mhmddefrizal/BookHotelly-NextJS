ac07073d6883c9a0d67df822a58dffdbe5098fdc
Oka Windana Saputra <okwan2004@gmail.com>
2025-11-28 16:27:19 +0700

    menambahkan /malam

--- web/components/card.tsx
+++ web/components/card.tsx
@@ -14,6 +14,7 @@ function Card() {
         </h4>
         <h4 className="text-2xl mb-7 ">
             <span className="font-semibold text-gray-500 ">Rp. 8500000</span>
+            <span>/malam</span>
         </h4>
       </div>
     </div>
