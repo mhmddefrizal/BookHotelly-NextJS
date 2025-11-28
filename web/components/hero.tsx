e1a5bb150b6018b008a2c2d448457082efd59145
mhmddefrizal <muhammaddefrizal08@gmail.com>
2025-11-28 14:13:06 +0700

    tambah button pesan sekarang

--- web/components/hero.tsx
+++ web/components/hero.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Hero = () => {
   return (
@@ -10,6 +11,11 @@ const Hero = () => {
       <div className="relative flex flex-col justify-center items-center h-full text-center">
         <h1 className="text-7xl font-extrabold leading-tight mb-3 capitalize">Booking ruang kamarmu!</h1>
         <p className="text-xl text-gray-300 mb-8">Dapatkan Penawaran Spesial hanyak Untukmu Hari Ini.</p>
+        <div className="flex gap-5">
+          <Link href="/kamar" className="bg-blue-600 text-white hover:bg-blue-900 py-2.5 px-6 md:px-10 text-lg font-semibold hover:scale-105 hover:shadow-lg">
+            Pesan Sekarang!
+          </Link>
+        </div>
       </div>
     </div>
   );
