2dc277f5900256d016c54d75ea08f49a63f38c78
Rivaldzz <rivaldosborn@gmail.com>
2025-11-28 21:04:35 +0700

    Update ke-1 Page sign in

--- web/app/masuk/page.tsx
+++ web/app/masuk/page.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const SignInPage = () => {
   return (
-    <div>SignInPage</div>
+    <div className='min-h-screen flex item-center'>
+        <div className="bg-white w-96 mx-auto rounded-sm shadow p-8">
+            <h1 className='text-4xl font-bold mb-1'>Masuk</h1>
+        </div>
+    </div>
   )
 }
 
