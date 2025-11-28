f8ffaffcec0dee4ecc12d6fb1bc6c5fd0bdb5439
Rivaldzz <rivaldosborn@gmail.com>
2025-11-28 19:40:49 +0700

    Penambahan bagian tentang kami

---
+++ web/app/about/page.tsx
@@ -0,0 +1,12 @@
+import HeaderSection from "@/components/header-section"
+
+const AboutPage = () => {
+  return (
+    <div>
+      <HeaderSection title="Tentang Kami" subTitle="Belum update" />
+      <div className="max-w-7xl mx-auto py-20 px-4"></div>
+    </div>
+  )
+}
+
+export default AboutPage
