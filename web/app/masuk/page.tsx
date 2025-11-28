d511ac18377175454e30d566cd666a9ffdd19d61
Rivaldzz <rivaldosborn@gmail.com>
2025-11-28 21:06:30 +0700

    Update ke-2 Page sign in

--- web/app/masuk/page.tsx
+++ web/app/masuk/page.tsx
@@ -5,6 +5,7 @@ const SignInPage = () => {
     <div className='min-h-screen flex item-center'>
         <div className="bg-white w-96 mx-auto rounded-sm shadow p-8">
             <h1 className='text-4xl font-bold mb-1'>Masuk</h1>
+            <p className='font-medium mb-5 text-gray-400'>Masuk menggunakan akun kamu</p>
         </div>
     </div>
   )
