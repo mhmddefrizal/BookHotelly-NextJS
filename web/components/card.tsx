6293ed4e5b49b57e8a6c5469bb5c43907fba7063
Oka Windana Saputra <okwan2004@gmail.com>
2025-11-28 15:46:37 +0700

    menambahkan container header

--- web/components/card.tsx
+++ web/components/card.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Card() {
   return (
     <div className="bg-white shadow-lg rounded-sm transition duration-100 hover:shadow-sm">
-    
+    <div className="h-[260px] w-auto rounded-t-sm relative"></div>
     </div>
   )
 }
