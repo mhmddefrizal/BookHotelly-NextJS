72041c0182e923339dd7aff316a190a017f45442
Rivaldzz <rivaldosborn@gmail.com>
2025-11-28 21:12:42 +0700

    Update ke-4 Page sign in

--- web/app/masuk/page.tsx
+++ web/app/masuk/page.tsx
@@ -3,7 +3,7 @@ import LoginButton from "@/components/login-button"
 const SignInPage = () => {
   return (
     <div className='min-h-screen flex item-center'>
-        <div className="bg-white w-96 mx-auto rounded-sm shadow p-8">
+        <div className="bg-white w-100 mx-auto rounded-sm shadow p-8">
             <h1 className='text-4xl font-bold mb-1'>Masuk</h1>
             <p className='font-medium mb-5 text-gray-400'>Masuk menggunakan akun kamu</p>
             <div className="py-4 text-center">
