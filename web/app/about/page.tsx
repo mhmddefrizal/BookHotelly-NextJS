8f6132379c4b5bbd658aef4e142ff086b46d1940
Rivaldzz <rivaldosborn@gmail.com>
2025-11-28 19:46:14 +0700

    Update gambar bagian tentang kami

--- web/app/about/page.tsx
+++ web/app/about/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import HeaderSection from "@/components/header-section"
 
 const AboutPage = () => {
@@ -5,7 +6,9 @@ const AboutPage = () => {
     <div>
       <HeaderSection title="Tentang Kami" subTitle="Belum update" />
       <div className="max-w-7xl mx-auto py-20 px-4">
-        <div className="grid md:grid-cols-2 gap-8"></div>
+        <div className="grid md:grid-cols-2 gap-8">
+            <Image src='/about-image.jpg' alt="tentang kami" width={650} height={579}/>
+        </div>
       </div>
     </div>
   )
