55318bf39b662259b4442148db3d48aec723ac47
Rivaldzz <rivaldosborn@gmail.com>
2025-11-28 19:57:40 +0700

    Update ke-4 bagian tentang kami

--- web/app/about/page.tsx
+++ web/app/about/page.tsx
@@ -11,6 +11,7 @@ const AboutPage = () => {
             <div>
                 <h1 className="text-5xl font-semibold text-gray-900 mb-4">Siapa Kami?</h1>
                 <p className="text-gray-700 py-5">Masukkan Teks Masukkan Teks Masukkan Teks Masukkan Teks Masukkan Teks Masukkan Teks Masukkan Teks</p>
+                <ul className="list-item space-y-6 pt-8 "></ul>
             </div>
         </div>
       </div>
