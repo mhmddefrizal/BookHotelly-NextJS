1f40349cfb41e8e44f586bb3fcfc12efb5f56d11
mhmddefrizal <muhammaddefrizal08@gmail.com>
2025-11-30 21:43:56 +0700

    item to items

--- web/app/masuk/page.tsx
+++ web/app/masuk/page.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
 
 const SignInPage = () => {
   return (
-    <div className="min-h-screen flex item-center">
+    <div className="min-h-screen flex items-center">
       <div className="bg-white w-96 mx-auto rounded-sm shadow p-8">
         <h1 className="text-4xl font-bold mb-1">Masuk</h1>
         <p className="font-medium mb-5 text-gray-500">Masuk menggunakan akun kamu</p>
