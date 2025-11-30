8ef2fa47b6ee7ebb99006d34db45e626bf33ab02
mhmddefrizal <muhammaddefrizal08@gmail.com>
2025-11-30 18:14:19 +0700

    change grip to grid

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div className="mt-16">
         <div className="text-center">
           <h1 className="text-4xl font-bold uppercase">Kamar dan Tarif</h1>
-          <p className="py-3">Pilih kamar sesuai kebutuhan dan kenyamanan keluarga anda tercinta.</p>
+          <p className="py-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, accusantium.</p>
         </div>
         <Main />
       </div>
--- web/components/main.tsx
+++ web/components/main.tsx
@@ -3,7 +3,7 @@ import Card from "@/components/card";
 const Main = () => {
   return (
     <div className="max-w-screen-xl py-6 pb-20 px-4 mx-auto">
-      <div className="grip gap-7 md:grid-cols-3">
+      <div className="grid gap-7 md:grid-cols-3">
         <Card />
         <Card />
         <Card />
