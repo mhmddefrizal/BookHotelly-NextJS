376652c22ee6955664232ec5ba9691b1d635ad5e
mhmddefrizal <muhammaddefrizal08@gmail.com>
2025-11-30 17:44:27 +0700

    penyesuaian styling part1

--- web/components/card.tsx
+++ web/components/card.tsx
@@ -1,29 +1,31 @@
 import Image from "next/image";
 import Link from "next/link";
-import {IoPeopleOutline} from "react-icons/io5"; 
+import { IoPeopleOutline } from "react-icons/io5";
 
 function Card() {
   return (
     <div className="bg-white shadow-lg rounded-sm transition duration-100 hover:shadow-sm">
       <div className="h-[260px] w-auto rounded-t-sm relative">
-        <Image src="/hero.jpg" width={345} height={240} alt="room image" className="w-full h-full
-        object-cover rounded-t-sm"/>
+        <Image src="/hero.jpg" width={345} height={240} alt="room image" className="w-full h-full object-cover rounded-t-sm" />
       </div>
       <div className="p-8">
         <h4 className="text-2xl font-medium">
-            <Link href="#" className="hover:text-gray-800 transition duration-100" >Kamar Mewah</Link>
+          <Link href="#" className="hover:text-gray-800 transition duration-150">
+            Kamar Mewah
+          </Link>
         </h4>
-        <h4 className="text-2xl mb-7 ">
-            <span className="font-semibold text-gray-500 ">Rp. 8500000</span>
-            <span className="text-gray-400 text-sm">/malam</span>
+        <h4 className="text-2xl mb-7">
+          <span className="font-semibold text-gray-600">Rp 2100000</span>
+          <span className="text-gray-400 text-sm">/Malam</span>
         </h4>
         <div className="flex items-center justify-between">
-            <div className="flex items-center space-x-2">
-              <IoPeopleOutline>
-                <span>2 Tamu</span>
-              </IoPeopleOutline>
-            </div>
-            <Link href="#" className="px-6 py-2.5 md:px-10 md:py-3 font-semibold bg-orange-300 rounded-sm hover:bg-orange-500 transition duration-100">Pesan Sekarang!</Link>
+          <div className="flex items-center space-x-2">
+            <IoPeopleOutline />
+            <span>2 Orang</span>
+          </div>
+          <Link href="#" className="px-6 py-2.5 md:px-10 md:py-3 font-semibold text-white bg-blue-600 rounded-sm hover:bg-blue-900 transition duration-150">
+            Pesan Sekarang!
+          </Link>
         </div>
       </div>
     </div>
