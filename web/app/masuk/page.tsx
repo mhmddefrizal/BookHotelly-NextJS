7b5aee5ec8a2c730586c4c3891b52fa9a2dc074f
mhmddefrizal <muhammaddefrizal08@gmail.com>
2025-11-30 21:43:26 +0700

    penyesuaian kode part2

--- web/app/masuk/page.tsx
+++ web/app/masuk/page.tsx
@@ -1,23 +1,22 @@
 import { Metadata } from "next";
-import LoginButton from "@/components/login-button"
+import LoginButton from "@/components/login-button";
 
 export const metadata: Metadata = {
-    title: "Masuk",
-    
-}
+  title: "Masuk",
+};
 
 const SignInPage = () => {
   return (
-    <div className='min-h-screen flex item-center'>
-        <div className="bg-white w-100 mx-auto rounded-sm shadow p-8">
-            <h1 className='text-4xl font-bold mb-1'>Masuk</h1>
-            <p className='font-medium mb-5 text-gray-400'>Masuk menggunakan akun kamu</p>
-            <div className="py-4 text-center">
-                <LoginButton />
-            </div>
+    <div className="min-h-screen flex item-center">
+      <div className="bg-white w-96 mx-auto rounded-sm shadow p-8">
+        <h1 className="text-4xl font-bold mb-1">Masuk</h1>
+        <p className="font-medium mb-5 text-gray-500">Masuk menggunakan akun kamu</p>
+        <div className="py-4 text-center">
+          <LoginButton />
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default SignInPage;
--- web/app/tentang/page.tsx
+++ web/app/tentang/page.tsx
@@ -1,48 +1,48 @@
-import { Metadata } from "next"
-import Image from "next/image"
-import HeaderSection from "@/components/header-section"
-import { IoEyeOutline, IoLocateOutline } from "react-icons/io5"
+import { Metadata } from "next";
+import Image from "next/image";
+import HeaderSection from "@/components/header-section";
+import { IoEyeOutline, IoLocateOutline } from "react-icons/io5";
 
 export const metadata: Metadata = {
-    title: "Tentang Kami",
-    description: "Situ Siapa?",
-}
+  title: "Tentang",
+  description: "Siapa Kami",
+};
 
 const AboutPage = () => {
   return (
     <div>
-      <HeaderSection title="Tentang Kami" subTitle="Belum update" />
-      <div className="max-w-7xl mx-auto py-20 px-4">
+      <HeaderSection title="Tentang Kami" subTitle="Lorem ipsum dolor sit amet." />
+      <div className="max-w-screen-xl mx-auto py-20 px-4">
         <div className="grid md:grid-cols-2 gap-8">
-            <Image src='/about-image.jpg' alt="tentang kami" width={650} height={579}/>
-            <div>
-                <h1 className="text-5xl font-semibold text-gray-900 mb-4">Siapa Kami?</h1>
-                <p className="text-gray-700 py-5">Masukkan Teks Masukkan Teks Masukkan Teks Masukkan Teks Masukkan Teks Masukkan Teks Masukkan Teks</p>
-                <ul className="list-item space-y-6 pt-8 ">
-                    <li className="flex gap-5">
-                        <div className="flex-none mt-1">
-                            <IoEyeOutline className="size-7 text-gray-900" />
-                        </div>
-                        <div className="flex-1">
-                            <h4 className="text-lg font-semibold mb-1">Visi :</h4>
-                            <p className="text-gray-600">Kami sangat berkomitmen untuk memberikan pelayanan terbaik kepada pelanggan kami. Meskipun permintaannya aneh-aneh</p>
-                        </div>
-                    </li>
-                    <li className="flex gap-5">
-                        <div className="flex-none mt-1">
-                            <IoLocateOutline className="size-7 text-gray-900" />
-                        </div>
-                        <div className="flex-1">
-                            <h4 className="text-lg font-semibold mb-1">Misi :</h4>
-                            <p className="text-gray-600">Tidak Dapat Bicara Sekarang, WhatsApp Saja</p>
-                        </div>
-                    </li>
-                </ul>
-            </div>
+          <Image src="/about-image.jpg" alt="tentang gambar" width={650} height={579} />
+          <div>
+            <h1 className="text-5xl font-semibold text-gray-900 mb-4">Siapa Kami ?</h1>
+            <p className="text-gray-700 py-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti natus necessitatibus dignissimos obcaecati hic ipsum reiciendis accusamus quo voluptas impedit!</p>
+            <ul className="list-item space-y-6 pt-8">
+              <li className="flex gap-5">
+                <div className="flex-none mt-1">
+                  <IoEyeOutline className="size-7" />
+                </div>
+                <div className="flex-1">
+                  <h4 className="text-lg font-semibold mb-1">Visi :</h4>
+                  <p className="text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quia, quos veritatis dolorem ratione alias.</p>
+                </div>
+              </li>
+              <li className="flex gap-5">
+                <div className="flex-none mt-1">
+                  <IoLocateOutline className="size-7" />
+                </div>
+                <div className="flex-1">
+                  <h4 className="text-lg font-semibold mb-1">Misi :</h4>
+                  <p className="text-gray-600">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima inventore architecto id quam corrupti vitae laudantium repellendus voluptas dolor rerum.</p>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AboutPage
+export default AboutPage;
