4d8b47133ae7c6ebe58cce4b826825355eb8fcec
mhmddefrizal <muhammaddefrizal08@gmail.com>
2025-12-02 21:06:19 +0700

    Implementasi tombol login Google menggunakan Server Action

--- web/components/login-button.tsx
+++ web/components/login-button.tsx
@@ -1,10 +1,17 @@
 import { FaG } from "react-icons/fa6";
-
+import { signIn } from "@/auth";
 export const LoginGoogleButton = () => {
   return (
-    <button className="flex items-center justify-center gap-2 w-full bg-blue-600 text-white font-medium py-3 px-6 text-base rounded-sm hover:bg-blue-800 cursor-pointer ">
-      <FaG className="size-6" />
-      Masuk dengan akun google
-    </button>
+    <form
+      action={async () => {
+        "use server";
+        await signIn("google");
+      }}
+    >
+      <button className="flex items-center justify-center gap-2 w-full bg-blue-600 text-white font-medium py-3 px-6 text-base rounded-sm hover:bg-blue-800 cursor-pointer ">
+        <FaG className="size-6" />
+        Masuk dengan akun google
+      </button>
+    </form>
   );
 };
