c56aa81f8f771d296fdf2dc6f05741024692648b
Rivaldzz <rivaldosborn@gmail.com>
2025-12-16 22:44:20 +0700

    penambahan hal. reservasi, admin dashboard, checkout

---
+++ web/app/reservasisaya/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HalamanReservasiSaya = () => {
+  return (
+    <div className='py-20 mt-10'>Halaman Reservasi Saya</div>
+  )
+}
+
+export default HalamanReservasiSaya
--- web/middleware.ts
+++ web/middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { auth } from "@/auth";
 
-const ProtectedRoutes = ["/myreservation", "/checkout", "/admin"];
+const ProtectedRoutes = ["/reservasisaya", "/checkout", "/admin"];
 export async function middleware(request: NextRequest) {
     const session = await auth();
     const isLoggedIn = !!session?.user;
