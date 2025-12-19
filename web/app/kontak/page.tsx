060506224c25c3a530394e84836fe690ac8bb426
mhmddefrizal <muhammaddefrizal08@gmail.com>
2025-12-19 08:15:35 +0700

    fix error kontakForm

--- web/app/kontak/page.tsx
+++ web/app/kontak/page.tsx
@@ -1,56 +1,59 @@
 import { Metadata } from "next";
 import HeaderSection from "@/components/header-section";
 import { IoCallOutline, IoMailOutline, IoLocationOutline } from "react-icons/io5";
-import kontakForm from "@/components/kontak-form";
+import KontakForm from "@/components/kontak-form";
 
 export const metadata: Metadata = {
-    title: "Contact"
-}
+  title: "Contact",
+};
 const HalamanKontak = () => {
-    return (
-        <div>
-            <HeaderSection title="Kontak Kami" subTitle="Jika ingin info lebih lanjut hubungi kami." />
-            <div className="max-w-screen-xl mx-auto py-20 px-4 ">
-                <div className="grid md:grid-cols-2 gap-8">
-                    <h1 className="text-lg text-gray-500 mb-3">Kontak Kami</h1>
-                    <h1 className="text-5xl font-semibold text-gray-900 mb-4">Hubungi kami segera.</h1>
-                    <p className="text-gray-700 py-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores quae eos cumque expedita perferendis sint beatae provident neque dolor similique doloremque sapiente rem, quia tempore minus maxime. Quae, culpa nobis!
-                        Voluptates, porro necessitatibus nulla, nihil magnam incidunt, minima expedita aut veritatis voluptas sed ipsam non? Delectus soluta perferendis iste nostrum fugiat ipsa, laudantium molestias assumenda neque vero cupiditate tempore ducimus.</p>
-                    <ul>
-                        <li className="flex gap-5">
-                            <div className="flex-none bg-gray-300 p-3 shadow-sm rounded-sm">
-                                <IoMailOutline className="size-7" />
-                            </div>
-                            <div className="flex-1">
-                                <h4 className="text-lg font-semibold mb-1">Email :</h4>
-                                <p>email-uwu@example.com</p>
-                            </div>
-                        </li>
-                        <li className="flex gap-5">
-                            <div className="flex-none bg-gray-300 p-3 shadow-sm rounded-sm">
-                                <IoCallOutline className="size-7" />
-                            </div>
-                            <div className="flex-1">
-                                <h4 className="text-lg font-semibold mb-1">Telepon :</h4>
-                                <p>+62 83902785927, masukkan teks</p>
-                            </div>
-                        </li>
-                        <li className="flex gap-5">
-                            <div className="flex-none bg-gray-300 p-3 shadow-sm rounded-sm">
-                                <IoLocationOutline className="size-7" />
-                            </div>
-                            <div className="flex-1">
-                                <h4 className="text-lg font-semibold mb-1">Lokasi :</h4>
-                                <p>Jl. Endang 12</p>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-                {/* Kontak formulir */}
-                <kontakForm />
-            </div>
+  return (
+    <div>
+      <HeaderSection title="Kontak Kami" subTitle="Jika ingin info lebih lanjut hubungi kami." />
+      <div className="max-w-screen-xl mx-auto py-20 px-4 ">
+        <div className="grid md:grid-cols-2 gap-8">
+          <h1 className="text-lg text-gray-500 mb-3">Kontak Kami</h1>
+          <h1 className="text-5xl font-semibold text-gray-900 mb-4">Hubungi kami segera.</h1>
+          <p className="text-gray-700 py-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores quae eos cumque expedita perferendis sint beatae provident neque dolor similique doloremque sapiente rem, quia tempore minus maxime. Quae, culpa nobis!
+            Voluptates, porro necessitatibus nulla, nihil magnam incidunt, minima expedita aut veritatis voluptas sed ipsam non? Delectus soluta perferendis iste nostrum fugiat ipsa, laudantium molestias assumenda neque vero cupiditate
+            tempore ducimus.
+          </p>
+          <ul>
+            <li className="flex gap-5">
+              <div className="flex-none bg-gray-300 p-3 shadow-sm rounded-sm">
+                <IoMailOutline className="size-7" />
+              </div>
+              <div className="flex-1">
+                <h4 className="text-lg font-semibold mb-1">Email :</h4>
+                <p>email-uwu@example.com</p>
+              </div>
+            </li>
+            <li className="flex gap-5">
+              <div className="flex-none bg-gray-300 p-3 shadow-sm rounded-sm">
+                <IoCallOutline className="size-7" />
+              </div>
+              <div className="flex-1">
+                <h4 className="text-lg font-semibold mb-1">Telepon :</h4>
+                <p>+62 83902785927, masukkan teks</p>
+              </div>
+            </li>
+            <li className="flex gap-5">
+              <div className="flex-none bg-gray-300 p-3 shadow-sm rounded-sm">
+                <IoLocationOutline className="size-7" />
+              </div>
+              <div className="flex-1">
+                <h4 className="text-lg font-semibold mb-1">Lokasi :</h4>
+                <p>Jl. Endang 12</p>
+              </div>
+            </li>
+          </ul>
         </div>
-    )
-}
+        {/* Kontak formulir */}
+        <KontakForm />
+      </div>
+    </div>
+  );
+};
 
-export default HalamanKontak
+export default HalamanKontak;
--- web/components/kontak-form.tsx
+++ web/components/kontak-form.tsx
@@ -1,93 +1,58 @@
-import React from 'react'
+import React from "react";
 
-const kontakForm = () => {
-    return (
-        <div className='bg-white p-8 rounded-sm shadow-sm'>
-            <form action="">
-                <div className="grid md:grid-cols-2 gap-7 mt-6">
-                    <div>
-                        <input
-                            type="text"
-                            name="name"
-                            className="bg-gray-50 p-3 border border-gray-200 rounded-sm w-full font-light"
-                            placeholder="Name"
-                        />
+const KontakForm = () => {
+  return (
+    <div className="bg-white p-8 rounded-sm shadow-sm">
+      <form action="">
+        <div className="grid md:grid-cols-2 gap-7 mt-6">
+          <div>
+            <input type="text" name="name" className="bg-gray-50 p-3 border border-gray-200 rounded-sm w-full font-light" placeholder="Name" />
 
-                        <div aria-live="polite" aria-atomic="true">
-                            <p className="text-sm text-red-500 mt-2">
-                                message
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div className="grid md:grid-cols-2 gap-7 mt-6">
-                    <div>
-                        <input
-                            type="email"
-                            name="email"
-                            className="bg-gray-50 p-3 border border-gray-200 rounded-sm w-full font-light"
-                            placeholder="Name"
-                        />
-
-                        <div aria-live="polite" aria-atomic="true">
-                            <p className="text-sm text-red-500 mt-2">
-                                gregours@example.com
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div className="grid md:grid-cols-2 gap-7 mt-6">
-                    <div>
-                        <input
-                            type="text"
-                            name="subject"
-                            className="bg-gray-50 p-3 border border-gray-200 rounded-sm w-full font-light"
-                            placeholder="Name"
-                        />
-
-                        <div aria-live="polite" aria-atomic="true">
-                            <p className="text-sm text-red-500 mt-2">
-                                subjek
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div className="grid md:grid-cols-2 gap-7 mt-6">
-                    <div className='md:col-span-2'>
-                        <input
-                            type="text"
-                            name="Subject"
-                            className="bg-gray-50 p-3 border border-gray-200 rounded-sm w-full font-light"
-                            placeholder="Subject*"
-                        />
-
-                        <div aria-live="polite" aria-atomic="true">
-                            <p className="text-sm text-red-500 mt-2">
-                                message
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div className="grid md:grid-cols-2 gap-7 mt-6">
-                    <div className='md:col-span-2'>
-                        <textarea
-                            name="message"
-                            rows={5}
-                            className="bg-gray-50 p-3 border border-gray-200 rounded-sm w-full font-light"
-                            placeholder="Tuliskan pesan/kesan disini...*"
-                        ></textarea>
-
-                        <div aria-live="polite" aria-atomic="true">
-                            <p className="text-sm text-red-500 mt-2">
-                                message
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <button type='submit' className='px-10 py-4 text-center font-semibold text-white w-full bg-orange-400 rounded-sm hover:bg-orange-500 cursor-pointer'></button>
-            </form>
+            <div aria-live="polite" aria-atomic="true">
+              <p className="text-sm text-red-500 mt-2">message</p>
+            </div>
+          </div>
         </div>
-    )
-}
+        <div className="grid md:grid-cols-2 gap-7 mt-6">
+          <div>
+            <input type="email" name="email" className="bg-gray-50 p-3 border border-gray-200 rounded-sm w-full font-light" placeholder="Name" />
 
-export default kontakForm
+            <div aria-live="polite" aria-atomic="true">
+              <p className="text-sm text-red-500 mt-2">gregours@example.com</p>
+            </div>
+          </div>
+        </div>
+        <div className="grid md:grid-cols-2 gap-7 mt-6">
+          <div>
+            <input type="text" name="subject" className="bg-gray-50 p-3 border border-gray-200 rounded-sm w-full font-light" placeholder="Name" />
+
+            <div aria-live="polite" aria-atomic="true">
+              <p className="text-sm text-red-500 mt-2">subjek</p>
+            </div>
+          </div>
+        </div>
+        <div className="grid md:grid-cols-2 gap-7 mt-6">
+          <div className="md:col-span-2">
+            <input type="text" name="Subject" className="bg-gray-50 p-3 border border-gray-200 rounded-sm w-full font-light" placeholder="Subject*" />
+
+            <div aria-live="polite" aria-atomic="true">
+              <p className="text-sm text-red-500 mt-2">message</p>
+            </div>
+          </div>
+        </div>
+        <div className="grid md:grid-cols-2 gap-7 mt-6">
+          <div className="md:col-span-2">
+            <textarea name="message" rows={5} className="bg-gray-50 p-3 border border-gray-200 rounded-sm w-full font-light" placeholder="Tuliskan pesan/kesan disini...*"></textarea>
+
+            <div aria-live="polite" aria-atomic="true">
+              <p className="text-sm text-red-500 mt-2">message</p>
+            </div>
+          </div>
+        </div>
+        <button type="submit" className="px-10 py-4 text-center font-semibold text-white w-full bg-orange-400 rounded-sm hover:bg-orange-500 cursor-pointer"></button>
+      </form>
+    </div>
+  );
+};
+
+export default KontakForm;
