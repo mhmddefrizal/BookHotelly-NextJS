122a0eee25e3288a80cbdc68fb19eaf32316a0b8
Oka Windana Saputra <okwan2004@gmail.com>
2025-12-20 10:00:07 +0700

    menambahkan email minimal 6 karakter dan format email

--- web/lib/zod.ts
+++ web/lib/zod.ts
@@ -2,4 +2,5 @@ import {object, string} from "zod";
 
 export const ContactSchema = object({
     name: string().min(6, "nama harus berisi 6 karakter"),
+    email: string().min(6, "email harus berisi 6 karakter").email("email tidak valid"),
 });
