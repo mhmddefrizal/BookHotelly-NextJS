eab63f35e65860a39480833e0bb81be9df0d5213
Oka Windana Saputra <okwan2004@gmail.com>
2025-12-20 10:03:01 +0700

    menambahkan pesan min 50 karakter dan 200 karater

--- web/lib/zod.ts
+++ web/lib/zod.ts
@@ -4,4 +4,5 @@ export const ContactSchema = object({
     name: string().min(6, "nama harus berisi 6 karakter"),
     email: string().min(6, "email harus berisi 6 karakter").email("email tidak valid"),
     subject : string().min(6, "subjek harus berisi 6 karakter"),
+    message : string().min(50, "pesan harus berisi 50 karakter").max(200, "pesan harus berisi maksimal 200 karakter")
 });
