2d00720d087a7ffb25e1eaa3f1f2c6703ebbfb5c
Oka Windana Saputra <okwan2004@gmail.com>
2025-12-20 10:16:56 +0700

    menambahkan handling error validasi

--- web/lib/action.ts
+++ web/lib/action.ts
@@ -4,4 +4,10 @@ import { ContactSchema } from "@/lib/zod";
 
 export const ContactMessage = async (formData: FormData) => {
     const validatedFields = ContactSchema.safeParse(Object.fromEntries(formData.entries()))
+
+    if(!validatedFields.success) {
+        return {
+            error: validatedFields.error.flatten().fieldErrors
+        };
+    }
 };
