a1a1c87e04aaf8ac1f9b11e54b9801bcc62af128
Oka Windana Saputra <okwan2004@gmail.com>
2025-12-20 10:22:27 +0700

    menyimpan pesan contact ke database

--- web/lib/action.ts
+++ web/lib/action.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { ContactSchema } from "@/lib/zod";
+import { prisma } from "@/lib/prisma";
 
 export const ContactMessage = async (formData: FormData) => {
     const validatedFields = ContactSchema.safeParse(Object.fromEntries(formData.entries()))
@@ -11,5 +12,18 @@ export const ContactMessage = async (formData: FormData) => {
         };
     }
     const {name, email, subject, message} = validatedFields.data;
+
+    try {
+        await prisma.contact.create({
+            data:{
+                name,
+                email,
+                subject,
+                message
+            }
+        })
+    }catch (error) {
+        
+    }
 };
 
