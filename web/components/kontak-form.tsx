3f377beadd6cc3fba7afd7c41ea6a2c136ac7c8a
mhmddefrizal <muhammaddefrizal08@gmail.com>
2025-12-27 00:49:00 +0700

    sesuaikan tata letak KontakForm part2

--- web/components/kontak-form.tsx
+++ web/components/kontak-form.tsx
@@ -16,41 +16,28 @@ const KontakForm = () => {
       <form action={FromAction}>
         <div className="grid md:grid-cols-2 gap-7 mt-6">
           <div>
-            <input type="text" name="name" className="bg-gray-50 p-3 border border-gray-200 rounded-sm w-full font-light" placeholder="Name" />
+            <input type="text" name="name" className="bg-gray-50 p-3 border border-gray-200 rounded-sm w-full font-light" placeholder="Nama*" />
 
             <div aria-live="polite" aria-atomic="true">
               <p className="text-sm text-red-500 mt-2">{state?.error?.name}</p>
             </div>
           </div>
-        </div>
-        <div className="grid md:grid-cols-2 gap-7 mt-6">
           <div>
-            <input type="email" name="email" className="bg-gray-50 p-3 border border-gray-200 rounded-sm w-full font-light" placeholder="Name" />
+            <input type="email" name="email" className="bg-gray-50 p-3 border border-gray-200 rounded-sm w-full font-light" placeholder="johndoe@example.com*" />
 
             <div aria-live="polite" aria-atomic="true">
               <p className="text-sm text-red-500 mt-2">{state?.error?.email}</p>
             </div>
           </div>
-        </div>
-        <div className="grid md:grid-cols-2 gap-7 mt-6">
-          <div>
-            <input type="text" name="subject" className="bg-gray-50 p-3 border border-gray-200 rounded-sm w-full font-light" placeholder="Name" />
+
+          <div className="md:col-span-2">
+            <input type="text" name="subject" className="bg-gray-50 p-3 border border-gray-200 rounded-sm w-full font-light" placeholder="Subject*" />
 
             <div aria-live="polite" aria-atomic="true">
               <p className="text-sm text-red-500 mt-2">{state?.error?.subject}</p>
             </div>
           </div>
-        </div>
-        <div className="grid md:grid-cols-2 gap-7 mt-6">
-          <div className="md:col-span-2">
-            <input type="text" name="Subject" className="bg-gray-50 p-3 border border-gray-200 rounded-sm w-full font-light" placeholder="Subject*" />
 
-            <div aria-live="polite" aria-atomic="true">
-              <p className="text-sm text-red-500 mt-2">{state?.error?.message}</p>
-            </div>
-          </div>
-        </div>
-        <div className="grid md:grid-cols-2 gap-7 mt-6">
           <div className="md:col-span-2">
             <textarea name="message" rows={5} className="bg-gray-50 p-3 border border-gray-200 rounded-sm w-full font-light" placeholder="Tuliskan pesan/kesan disini...*"></textarea>
 
@@ -59,7 +46,10 @@ const KontakForm = () => {
             </div>
           </div>
         </div>
-        <button type="submit" className="px-10 py-4 text-center font-semibold text-white w-full bg-orange-400 rounded-sm hover:bg-orange-500 cursor-pointer"></button>
+
+        <button type="submit" className="px-10 py-4 text-center font-semibold text-white w-full bg-blue-600 rounded-sm hover:bg-blue-900 cursor-pointer">
+          Kirim Pesan
+        </button>
       </form>
     </div>
   );
--- web/lib/zod.ts
+++ web/lib/zod.ts
@@ -1,8 +1,8 @@
-import {object, string} from "zod";
+import { object, string } from "zod";
 
 export const ContactSchema = object({
-    name: string().min(6, "nama harus berisi 6 karakter"),
-    email: string().min(6, "email harus berisi 6 karakter").email("email tidak valid"),
-    subject : string().min(6, "subjek harus berisi 6 karakter"),
-    message : string().min(50, "pesan harus berisi 50 karakter").max(200, "pesan harus berisi maksimal 200 karakter")
+  name: string().min(6, "nama harus berisi 6 karakter"),
+  email: string().min(6, "email harus berisi 6 karakter.").email(" Mohon masukkan email yang valid"),
+  subject: string().min(6, "subjek harus berisi 6 karakter"),
+  message: string().min(50, "pesan harus berisi 50 karakter").max(200, "pesan harus berisi maksimal 200 karakter"),
 });
