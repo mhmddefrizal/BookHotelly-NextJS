a64d4f39615aee2fdd79df265d64436daa03e37b
mhmddefrizal <muhammaddefrizal08@gmail.com>
2025-12-30 23:45:16 +0700

    import function component BuatForm dan render

--- web/app/admin/kamar/buat/page.tsx
+++ web/app/admin/kamar/buat/page.tsx
@@ -1,5 +1,11 @@
+import BuatForm from "@/components/admin/kamar/buat-form";
+
 const BuatHalamanKamar = () => {
-  return <div className="max-w-screen-xl px-4 py-16 mt-10 mx-auto"></div>;
+  return (
+    <div className="max-w-screen-xl px-4 py-16 mt-10 mx-auto">
+      <BuatForm />
+    </div>
+  );
 };
 
 export default BuatHalamanKamar;
