f67905b6155463b54c907009adacfda244af63c3
mhmddefrizal <muhammaddefrizal08@gmail.com>
2026-01-01 23:53:36 +0700

    import functional component buat kamar dan render

--- web/app/admin/kamar/buat/page.tsx
+++ web/app/admin/kamar/buat/page.tsx
@@ -1,9 +1,9 @@
-import BuatForm from "@/components/admin/kamar/buat-form";
+import BuatKamar from "@/components/admin/kamar/buat-kamar";
 
 const BuatHalamanKamar = () => {
   return (
     <div className="max-w-screen-xl px-4 py-16 mt-10 mx-auto">
-      <BuatForm />
+      <BuatKamar />
     </div>
   );
 };
