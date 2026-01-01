03b0ca7b459e0a8c29d272f1d97e7ea81674f7d4
mhmddefrizal <muhammaddefrizal08@gmail.com>
2026-01-01 23:51:35 +0700

    buat file baru buat kamar tsx untuk fetching data

---
+++ web/components/admin/kamar/buat-kamar.tsx
@@ -0,0 +1,12 @@
+import BuatForm from "@/components/admin/kamar/buat-form";
+
+const BuatKamar = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-gray-800 mb-4">Buat Kamar Baru</h1>
+      <BuatForm />
+    </div>
+  );
+};
+
+export default BuatKamar;
