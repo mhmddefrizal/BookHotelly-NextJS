3898ec26d4f473d62b3beeda1ff72a9034d3c3dd
mhmddefrizal <muhammaddefrizal08@gmail.com>
2026-01-03 00:30:19 +0700

    validasi file route ts upload

--- web/app/api/upload/route.ts
+++ web/app/api/upload/route.ts
@@ -4,4 +4,15 @@ import { NextResponse } from "next/server";
 // buat API route untuk meng-handle upload file
 export const PUT = async (request: Request) => {
   const form = await request.formData();
+  const file = form.get("file") as File;
+
+  if (file.size === 0 || file.size === undefined) {
+    return NextResponse.json({ error: "File is required" }, { status: 400 });
+  }
+  if (file.size > 4000000) {
+    return NextResponse.json({ error: "File must be less than 4MB" }, { status: 400 });
+  }
+  if (!file.type.startsWith("image/")) {
+    return NextResponse.json({ error: "File must be an image" }, { status: 400 });
+  }
 };
