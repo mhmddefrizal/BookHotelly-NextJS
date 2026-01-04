bc9882027362eeeea056ae12fc299929b58f711b
mhmddefrizal <muhammaddefrizal08@gmail.com>
2026-01-05 00:24:59 +0700

    feat: mengambil data amenities dan meneruskannya ke komponen BuatForm

--- web/components/admin/kamar/buat-kamar.tsx
+++ web/components/admin/kamar/buat-kamar.tsx
@@ -1,10 +1,12 @@
 import BuatForm from "@/components/admin/kamar/buat-form";
-
-const BuatKamar = () => {
+import { getAmenities } from "@/lib/data";
+const BuatKamar = async () => {
+  const amenities = await getAmenities();
+  if (!amenities) return null;
   return (
     <div>
       <h1 className="text-3xl font-bold text-gray-800 mb-4">Buat Kamar Baru</h1>
-      <BuatForm />
+      <BuatForm amenities={amenities} />
     </div>
   );
 };
