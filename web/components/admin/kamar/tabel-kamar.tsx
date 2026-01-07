6c12a375ae1660dd71a6cf6e9f3a34fc992b4e20
Rivaldzz <rivaldosborn@gmail.com>
2026-01-07 19:17:16 +0700

    Tambah hal. tabel data kamar

---
+++ web/components/admin/kamar/tabel-kamar.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const TabelKamar = () => {
+  return (
+    // tabel kamar
+    <div className='bg-white p-4 mt-5 shadow-sm'>
+        <table>
+            <thead>
+                <tr>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+  )
+}
+
+export default TabelKamar
