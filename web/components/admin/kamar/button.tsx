ba447c21a05907d68263d73f594d4196d9eee620
Rivaldzz <rivaldosborn@gmail.com>
2026-01-07 20:55:48 +0700

    tambah tombol untuk tindakan edit daftar kamar

--- web/components/admin/kamar/button.tsx
+++ web/components/admin/kamar/button.tsx
@@ -1,5 +1,9 @@
 import { IoMdTrash } from "react-icons/io";
+import { MdEdit } from "react-icons/md";
 import { DeleteRoom } from "@/lib/action";
+import Link from "next/link";
+
+// tombol hapus kamar
 export const DeleteButton = ({ id, image }: { id: string; image: string }) => {
     const DeleteRoomWithId = DeleteRoom.bind(null, id, image);
     return (
@@ -11,5 +15,13 @@ export const DeleteButton = ({ id, image }: { id: string; image: string }) => {
                 <IoMdTrash size={5} color="red" />
             </button>
         </form >
+    );
+};
+
+export const EditButton = ({ id }: { id: string}) => {
+    return (
+        <Link href={`/admin/kamar/edit/${id}`} className="rounded-sm p-1 hover:bg-gray-200 cursor-pointer">
+                <MdEdit size={20} color="blue" />
+        </Link>    
     );
 };
