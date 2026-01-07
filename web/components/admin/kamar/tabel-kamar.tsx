378b7cc2ab4978a079d9dcb5dd8fe910fa99acf3
Rivaldzz <rivaldosborn@gmail.com>
2026-01-07 19:54:38 +0700

    fungsi isi tabel

--- web/components/admin/kamar/tabel-kamar.tsx
+++ web/components/admin/kamar/tabel-kamar.tsx
@@ -1,31 +1,39 @@
-import React from 'react'
+import { getRooms } from "@/lib/data";
+import Image from "next/image";
 
-const TabelKamar = () => {
-  return (
-    // tabel kamar
-    <div className='bg-white p-4 mt-5 shadow-sm'>
-        <table className='w-full divide-y divide-gray-300'>
-            <thead>
-                <tr>
-                    <th className='px-6 py-3 w-32 text-sm font-bold text-gray-700 uppercase text-left'>Gambar Kamar</th>
-                    <th className='px-6 py-3 w-32 text-sm font-bold text-gray-700 uppercase text-left'>Nama Kamar</th>
-                    <th className='px-6 py-3 w-32 text-sm font-bold text-gray-700 uppercase text-left'>Harga</th>
-                    <th className='px-6 py-3 w-32 text-sm font-bold text-gray-700 uppercase text-left'>Dibuat Sejak</th>
-                    <th className='px-6 py-3 w-32 text-sm font-bold text-gray-700 uppercase'>Ket.</th>
-                </tr>
-            </thead>
-            <tbody className='divide-y Idivide-gray-200'>
-                <tr className= 'hover:bg-gray-100'>
-                    <td className='px-6 py-4'></td>
-                    <td className='px-6 py-4'></td>
-                    <td className='px-6 py-4'></td>
-                    <td className='px-6 py-4'></td>
-                    <td className='px-6 py-4 text-right'></td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-  )
+const TabelKamar = async () => {
+    const rooms = await getRooms();
+    if (!rooms?.length) return <p>Tidak Ditemukan kamar</p>;
+    return (
+        // tabel kamar
+        <div className='bg-white p-4 mt-5 shadow-sm'>
+            <table className='w-full divide-y divide-gray-300'>
+                <thead>
+                    <tr>
+                        <th className='px-6 py-3 w-32 text-sm font-bold text-gray-700 uppercase text-left'>Gambar Kamar</th>
+                        <th className='px-6 py-3 w-32 text-sm font-bold text-gray-700 uppercase text-left'>Nama Kamar</th>
+                        <th className='px-6 py-3 w-32 text-sm font-bold text-gray-700 uppercase text-left'>Harga</th>
+                        <th className='px-6 py-3 w-32 text-sm font-bold text-gray-700 uppercase text-left'>Dibuat Sejak</th>
+                        <th className='px-6 py-3 w-32 text-sm font-bold text-gray-700 uppercase'>Ket.</th>
+                    </tr>
+                </thead>
+                <tbody className='divide-y Idivide-gray-200'>
+                    {/* isi tabel */}
+                    {rooms.map((room) => (
+                        <tr key={room.id} className='hover:bg-gray-100'>
+                            <td className='px-6 py-4'>
+                                <Image src={room.image} fill sizes="20vw" alt=" room image" className="object-cover" />
+                            </td>
+                            <td className='px-6 py-4'>{room.name}</td>
+                            <td className='px-6 py-4'>{room.price}</td>
+                            <td className='px-6 py-4'>{room.createdAt.toString()}</td>
+                            <td className='px-6 py-4 text-right'></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default TabelKamar
