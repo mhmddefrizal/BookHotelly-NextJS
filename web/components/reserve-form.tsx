444b3d1de4367fd0c57b44ee469e46dc164898b0
Oka Windana Saputra <okwan2004@gmail.com>
2026-01-08 13:47:04 +0700

    menambahkan atribut di DatePicker

--- web/components/reserve-form.tsx
+++ web/components/reserve-form.tsx
@@ -7,7 +7,12 @@ const reserveform = () => {
         <form action="">
             <div className="mb-4">
                 <label className="block mb-2 text-sm font-medium text-gray-800">Kedatangan - Keberangkatan</label>
-
+                <DatePicker
+                selected={}
+                startDate={}
+                endDate={}
+                minDate={}
+                selectsRange={true}/>
             </div>
         </form>
     </div>
