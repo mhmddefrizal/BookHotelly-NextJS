35d317af9f67c553826ed64bb1cea6e011e27544
Rivaldzz <rivaldosborn@gmail.com>
2026-01-08 20:50:09 +0700

    tampilkan detail checkout

--- web/app/checkout/[id]/page.tsx
+++ web/app/checkout/[id]/page.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import CheckoutDetail from '@/components/checkout-detail'
 
-const CheckoutPage = () => {
+// halaman checkout
+const CheckoutPage = async ({params}: {params: Promise<{id: string}>}) => {
+    // mengambil ID reservasi dari parameter URL
+    const reservationId = (await params).id;
   return (
+    // tampilkan detail checkout
     <div className='max-w-screen-xl px-4 mx-auto py-20 mt-12'>
         <h1>Checkout</h1>
+        <CheckoutDetail reservationId={reservationId} />
     </div>
   )
 }
