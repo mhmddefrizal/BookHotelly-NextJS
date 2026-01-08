5581fa03433af183970b47c7d32b35bfe2ad28fa
Rivaldzz <rivaldosborn@gmail.com>
2026-01-08 20:20:29 +0700

    tambah fungsi jika data reservasi tidak ditemukan, tampilkan pesan

--- web/components/checkout-detail.tsx
+++ web/components/checkout-detail.tsx
@@ -1,9 +1,21 @@
+import Image from 'next/image'
 import React from 'react'
 
-const CheckoutDetail = () => {
+// import fungsi untuk mengambil data reservasi berdasarkan ID
+const CheckoutDetail = async({reservationId}:{reservationId:string}) => {
+    const reservation = await getReservationById(reservationId);
+    // jika data reservasi tidak ditemukan, tampilkan pesan
+    if (!reservation || !reservation.Payment) return <h1>Tidak Ditemukan Data</h1>
   return (
-    <div className=''>
-        <div className=""></div>
+    <div className='grid md:grid-cols-2 gap-5'>
+        <div className="order-2">
+            <div className="flex flex-col mb-3 items-start bg-white border border-gray-200 rounded-sm md:flex-row md:w-full">
+                {/* menampilkan gambar kamar */}
+                <div className="aspect-video relative">
+                    <Image src={reservation.Room.image} width={500} height={300} alt={reservation.Room.name} fill className="object-cover w-full rounded-t-sm aspect-video md:rounded-t-none md:rounded-s -sm"/>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
