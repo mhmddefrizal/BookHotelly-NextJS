c3e4774348e0b3782c0aa434a2bbec063f467f1f
Rivaldzz <rivaldosborn@gmail.com>
2026-01-08 11:20:00 +0700

    implementasi fungsi data pada page kamar

--- web/components/card.tsx
+++ web/components/card.tsx
@@ -1,21 +1,22 @@
 import Image from "next/image";
 import Link from "next/link";
 import { IoPeopleOutline } from "react-icons/io5";
+import { Room } from "@prisma/client";
 
-const Card = () => {
+const Card = ({room}: {room:Room}) => {
   return (
     <div className="bg-white shadow-lg rounded-sm transition duration-100 hover:shadow-sm">
       <div className="h-[260px] w-auto rounded-t-sm relative">
-        <Image src="/hero.jpg" width={345} height={240} alt="room image" className="w-full h-full object-cover rounded-t-sm" />
+        <Image src={room.image} width={345} height={240} alt="room image" className="w-full h-full object-cover rounded-t-sm" />
       </div>
       <div className="p-8">
         <h4 className="text-2xl font-medium">
-          <Link href="#" className="hover:text-gray-800 transition duration-150">
-            Kamar Mewah
+          <Link href={`/kamar/${room.id}`} className="hover:text-gray-800 transition duration-150">
+            {room.name}
           </Link>
         </h4>
         <h4 className="text-2xl mb-7">
-          <span className="font-semibold text-gray-600">Rp 2100000</span>
+          <span className="font-semibold text-gray-600">{room.price}</span>
           <span className="text-gray-400 text-sm">/Malam</span>
         </h4>
         <div className="flex items-center justify-between">
--- web/components/main.tsx
+++ web/components/main.tsx
@@ -1,15 +1,15 @@
 import Card from "@/components/card";
+import { getRooms } from "@/lib/data";
 
-const Main = () => {
+const Main = async () => {
+  const rooms = await getRooms();
+  if (!rooms) return <p>No rooms available.</p>;
   return (
     <div className="max-w-screen-xl py-6 pb-20 px-4 mx-auto">
       <div className="grid gap-7 md:grid-cols-3">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {rooms.map((room) => (
+        <Card key={room.id} room={room} />
+        ))}
       </div>
     </div>
   );
