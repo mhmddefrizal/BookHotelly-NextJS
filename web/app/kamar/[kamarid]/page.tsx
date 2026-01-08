a38e5bd34107d9d7304a9083e02ec4f5f8d15554
Oka Windana Saputra <okwan2004@gmail.com>
2026-01-08 13:22:40 +0700

    import suspend

--- web/app/kamar/[kamarid]/page.tsx
+++ web/app/kamar/[kamarid]/page.tsx
@@ -1,18 +1,19 @@
 import { Metadata } from "next"
 import DetailKamar from "@/components/detail-kamar";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
-    title: "Kamar Detail",
+    title: "Detail Kamar",
 }
 
 const KamarDetailpage
  = async({params
 }:{params: Promise<{ kamarId: string }>}
  ) => {
-    const kamarId = (await.params).kamarId;
+    const kamarId = (await params).kamarId;
   return (
-    <div>KamarDetailpage
-
+    <div className="mt-16">
+        <DetailKamar roomId={kamarId} />
     </div>
   )
 }
