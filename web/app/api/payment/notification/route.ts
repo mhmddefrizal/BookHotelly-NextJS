349bfc03f2e4a0e86ce740487d2dd827366d1993
Rivaldzz <rivaldosborn@gmail.com>
2026-01-09 11:15:00 +0700

    validasi signature key

--- web/app/api/payment/notification/route.ts
+++ web/app/api/payment/notification/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import Prisma from "@/lib/prisma";
 import { PaymentProps } from "@/types/payment";
+import crypto from "crypto";
 
 // membuat endpoint untuk menerima notifikasi pembayaran dari Midtrans
 export const POST = async (request: Request) => {
@@ -16,4 +17,9 @@ export const POST = async (request: Request) => {
     const statusCode = data.status_code;
     const grossAmount = data.gross_amount;
     const signatureKey = data.signature_key;
+
+    // validasi signature key
+    const hash = crypto.createHash('sha512').update(
+        `${reservationId}${statusCode}${grossAmount}${process.env.MIDTRANS_SERVER_KEY}`
+    ).digest('hex');
 }
