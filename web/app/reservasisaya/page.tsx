2f3265401545a7d988c1cab0ae85de2362da19aa
Rivaldzz <rivaldosborn@gmail.com>
2026-01-09 14:04:09 +0700

    Update Page Reservasi Saya

--- web/app/reservasisaya/page.tsx
+++ web/app/reservasisaya/page.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import MyReserveList from '@/components/my-reserve-list'
 
+// Update Page Reservasi Saya
 const HalamanReservasiSaya = () => {
   return (
-    <div className='py-20 mt-10'>Halaman Reservasi Saya</div>
+    <div className="min-h-screen bg-slate-50">
+      <div className="max-w-screen-lg mx-auto mt-10 py-20 px-4">
+        <div className="flex items-center justify-between">
+          <div>
+            <h3 className="text-xl text-gray-800 mt-2">Woyy,</h3>
+            <p className="mt-1 font-medium mb-4">
+              Berikut adalah riwayat pemesanan Anda :
+            </p>
+          </div>
+        </div>
+
+        <div className="rounded-sm">
+          <MyReserveList />
+        </div>
+      </div>
+    </div>
   )
 }
 
