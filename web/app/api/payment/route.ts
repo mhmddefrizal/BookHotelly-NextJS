6400ba0e5b9327b31544f6f4c446cdb6b7929b2a
Rivaldzz <rivaldosborn@gmail.com>
2026-01-09 10:44:46 +0700

    buat parameter untuk transaksi Midtrans

--- web/app/api/payment/route.ts
+++ web/app/api/payment/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import Midtrans from "midtrans-client";
+import { reservationProps } from "@/types/reservation";
 
 // Create Snap API instance
 const snap = new Midtrans.Snap({
@@ -10,5 +11,23 @@ const snap = new Midtrans.Snap({
 
 // Handle POST request to create a payment transaction
 export const POST = async (request: Request) => {
-    const reservation = await request.json();
-}
+    const reservation: reservationProps = await request.json();
+
+    // buat parameter untuk transaksi Midtrans
+    const parameter = {
+        transaction_details: {
+            order_id: reservation.id,
+            gross_amount: reservation.Payment?.amount || 0,
+        },
+        credit_card: {
+            secure: true,
+        },
+        customer_details: {
+            first_name: reservation.User.name,
+            email: reservation.User.email,
+            phone: reservation.User.phone,
+        },
+    };
+
+    const token = await snap.createTransactionToken(parameter);
+    return NextResponse.json({ token });
