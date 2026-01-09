a96611cc457760845d65732bb79054f6a699bb39
Rivaldzz <rivaldosborn@gmail.com>
2026-01-09 11:23:07 +0700

    berikan respon sesuai status pembayaran

--- web/app/api/payment/notification/route.ts
+++ web/app/api/payment/notification/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import Prisma from "@/lib/prisma";
+import { prisma } from "@/lib/prisma";
 import { PaymentProps } from "@/types/payment";
 import crypto from "crypto";
 
@@ -22,4 +22,53 @@ export const POST = async (request: Request) => {
     const hash = crypto.createHash('sha512').update(
         `${reservationId}${statusCode}${grossAmount}${process.env.MIDTRANS_SERVER_KEY}`
     ).digest('hex');
-}
+
+    // berikan respon sesuai status pembayaran
+    // jika signature key tidak valid, kembalikan error
+
+    if (signatureKey !== hash) {
+        return NextResponse.json({ error: "Kode signature tidak valid" }, { status: 400 });
+    }
+
+    // proses update status pembayaran berdasarkan status transaksi
+    if (transactionStatus === 'capture') {
+        if (fraudStatus === 'accept') {
+            const transaction = await prisma.payment.update({
+                data: {
+                    method: paymentType,
+                    status: 'paid',
+                },
+                where: { reservationId }
+            });
+            responseData = transaction;
+        }
+    } else if (transactionStatus === 'settlement') {
+        const transaction = await prisma.payment.update({
+            data: {
+                method: paymentType,
+                status: 'paid',
+            },
+            where: { reservationId }
+        });
+        responseData = transaction;
+    } else if (transactionStatus === 'cancel' || transactionStatus === 'deny' || transactionStatus === 'expire') {
+        const transaction = await prisma.payment.update({
+            data: {
+                method: paymentType,
+                status: 'failed',
+            },
+            where: { reservationId }
+        });
+        responseData = transaction;
+    } else if (transactionStatus === 'pending') {
+        const transaction = await prisma.payment.update({
+            data: {
+                method: paymentType,
+                status: 'pending',
+            },
+            where: { reservationId }
+        });
+        responseData = transaction;
+    }
+    return NextResponse.json({responseData}, { status: 200 });
+};
