c1bce1d96a6e69ac0492a9e9889d35576003d469
Rivaldzz <rivaldosborn@gmail.com>
2026-01-09 10:48:44 +0700

    buat tombol pembayaran

---
+++ web/components/payment-button.tsx
@@ -0,0 +1,11 @@
+
+// tombol pembayaran
+const PaymentButton = () => {
+  return (
+    <button className="px-10 py-4 mt-2 text-center font-semibold bg-blue-600 text-white w-full rounded-sm hover:bg-blue-700 cursor-pointer">
+        Bayar Sekarang
+    </button>
+  )
+}
+
+export default PaymentButton
