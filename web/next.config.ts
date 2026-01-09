02db07bc3c3fea23f386635c5036cd9edcca1f82
Rivaldzz <rivaldosborn@gmail.com>
2026-01-09 11:30:25 +0700

    config untuk menambahkan header pada endpoint notifikasi pembayaran

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  // config untuk menambahkan header pada endpoint notifikasi pembayaran
+  async headers() {
+    return [
+      {
+        source: "api/payment/notification/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*"},
+          { key: "Access-Control-Allow-Methods", value: "GET, POST" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ];
+  },
+
   images: {
     remotePatterns: [
       {
