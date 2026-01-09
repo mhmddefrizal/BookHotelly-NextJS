0cc440040720247cb2c9e17826bcc2db8f971904
Rivaldzz <rivaldosborn@gmail.com>
2026-01-09 13:50:42 +0700

    membuat content pemesanan

--- web/components/my-reserve-list.tsx
+++ web/components/my-reserve-list.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import { getReservationByUserId } from '@/lib/data';
 import { notFound } from 'next/navigation';
 import { formatCurrency, formatDate } from '@/lib/utils';
+import { differenceInCalendarDays, DifferenceInCalendarDays } from 'date-fns';
 
 // membuat komponen MyReserveList
 const MyReserveList = async () => {
@@ -20,7 +21,7 @@ const MyReserveList = async () => {
 
                             <div className="flex gap-1 px-3 py-2 text-sm font-normal">
                                 <span>Status:</span>
-                                <span className="font-bold uppercase">status</span>
+                                <span className="font-bold uppercase">{item.Payment?.status}</span>
                             </div>
                         </div>
                     </div>
@@ -28,40 +29,43 @@ const MyReserveList = async () => {
                     <div className="flex flex-col mb-4 items-start bg-white rounded-sm md:flex-row md:w-full">
                         {/* content */}
                         <Image
-                            src={}
+                            src={item.Room.image}
                             width={500}
                             height={300}
                             className="object-cover w-full rounded-t-sm h-60 md:h-auto md:w-1/3 md:rounded-none md:rounded-s-sm"
                             alt="image room"
                         />
-
+                        {/* content pemesanan */}
                         <div className="flex items-center gap-1 mb-3 font-normal text-gray-700">
                             <div className="w-full">
                                 <div className="flex items-center justify-between text-sm font-medium text-gray-900 truncate">
                                     <span>Harga</span>
-                                    <span>Harga</span>
+                                    <span>{formatCurrency(item.Room.price)}</span>
                                 </div>
 
                                 <div className="flex items-center justify-between text-sm font-medium text-gray-900 truncate">
                                     <span>Kedatangan</span>
-                                    <span>Kedatangan</span>
+                                    <span>{formatDate(item.startDate.toISOString())}</span>
                                 </div>
 
                                 <div className="flex items-center justify-between text-sm font-medium text-gray-900 truncate">
                                     <span>Kepergian</span>
-                                    <span>Kepergian</span>
+                                    <span>{formatDate(item.endDate.toISOString())}</span>
                                 </div>
                                 <div className="flex items-center justify-between text-sm font-medium text-gray-900 truncate">
                                     <span>Durasi</span>
-                                    <span>Durasi</span>
+                                    <span>{differenceInCalendarDays(item.endDate, item.startDate)} 
+                                        <span className='ml-1'>Malam</span> 
+                                    </span>
                                 </div>
                                 <div className="flex items-center justify-between text-sm font-medium text-gray-900 truncate">
                                     <span>Sub Total</span>
-                                    <span>Sub Total</span>
+                                    <span>{item.Payment && formatCurrency(item.Payment.amount)}</span>
                                 </div>
                             </div>
                         </div>
                     </div>
+                    <div className=""></div>
                 </div>
             ))}
         </div>
