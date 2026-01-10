3c2930d4f294c315f7f32751d81e4f703c449231
Oka Windana Saputra <okwan2004@gmail.com>
2026-01-10 08:53:04 +0700

    menambahkan list tanggal buku (sementara)

--- web/components/reservasi-detail.tsx
+++ web/components/reservasi-detail.tsx
@@ -15,6 +15,16 @@ const ReservasiDetail = ({reseservasiId} : {reseservasiId: string}) => {
                         </div>
                     </div>
                 </li>
+                <li className='py-2'>
+                    <div className="flex items-center">
+                        <div className="flex-1 min-w-0 ms-4">
+                            <p className='text-sm font-medium text-gray-800 truncate'>Tanggal buku</p>
+                        </div>
+                        <div className='inline-flex items-center text-base font-semibold text-gray-900'>
+                            #Tanggal Buku
+                        </div>
+                    </div>
+                </li>
             </ul>
         </div>
     </div>
