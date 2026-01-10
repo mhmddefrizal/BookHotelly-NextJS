ac77859e034a75ac48d90c784b3b81b9d9054950
Rivaldzz <rivaldosborn@gmail.com>
2026-01-10 12:56:59 +0700

    buat card untuk menampilkan total pemesanan

--- web/components/admin/dashboard-cards.tsx
+++ web/components/admin/dashboard-cards.tsx
@@ -4,6 +4,7 @@ import {LuChartArea, LuShoppingCart, LuUser} from 'react-icons/lu'
 const DashboardCards = () => {
     return (
         <div className="grid md:grid-cols-3 gap-5 pb-10">
+            {/* card untuk menampilkan total penjualan */}
             <div className="flex items-center bg-white border rounded-md overflow-hidden shadow-sm">
                 <div className="p-4 bg-green-400">
                     {/* content */}
@@ -14,6 +15,18 @@ const DashboardCards = () => {
                     <p className='text-3xl'>999</p>
                 </div>
             </div>
+            {/* card untuk menampilkan total pemesanan */}
+            <div className="flex items-center bg-white border rounded-md overflow-hidden shadow-sm">
+                <div className="p-4 bg-green-400">
+                    {/* content */}
+                    <LuShoppingCart className="text-white" size={12} />
+                </div>
+                <div className="px-4 text-gray-400">
+                    <h3 className='text-sm tracking-winder'>Total Pemesanan</h3>
+                    <p className='text-3xl'>999</p>
+                </div>
+            </div>
+            
         </div>
     )
 }
