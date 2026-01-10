5ec7f1368f290fb36227440dea71243f5d4b962c
Oka Windana Saputra <okwan2004@gmail.com>
2026-01-10 08:39:31 +0700

    buat komponen ReservasiDetail

---
+++ web/components/reservasi-detail.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ReservasiDetail = ({reseservasiId} : {reseservasiId: string}) => {
+  return (
+    <div>ReservasiDetail</div>
+  )
+}
+
+export default ReservasiDetail
