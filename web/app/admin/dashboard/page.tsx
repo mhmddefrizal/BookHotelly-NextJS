ffabac9968102685dfa439683202efd538d7c0d9
Rivaldzz <rivaldosborn@gmail.com>
2026-01-10 13:41:44 +0700

    update dashboard

--- web/app/admin/dashboard/page.tsx
+++ web/app/admin/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from 'next';
 import DashboardCards from '@/components/admin/dashboard-cards';
+import ReservationList from '@/components/admin/reservation-list';
 import { Suspense } from 'react';
 
 // Metadata untuk halaman dashboard admin
@@ -12,8 +13,13 @@ const HalamanDashboard = () => {
   return (
     <div className='max-w-screen-xl px-4 py-16 mt-10 mx-auto'>
       <h1 className='text-4xl font-bold text-gray-500'>Dashboard</h1>
+      {/* tampilkan card dan daftar reservasi */}
       <Suspense fallback={<p>Loading...</p>}>
         <DashboardCards />
+      </Suspense>
+      {/* tampilkan daftar reservasi */}
+      <Suspense fallback={<p>Loading...</p>}>
+        <ReservationList />
       </Suspense>
     </div>
   )
