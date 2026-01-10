ad3e24876b9a4c546edb6d6112dfc012196d8c43
Oka Windana Saputra <okwan2004@gmail.com>
2026-01-10 08:32:20 +0700

    menyeiapkan layout halaman reservasi

--- web/app/reservasisaya/[id]/page.tsx
+++ web/app/reservasisaya/[id]/page.tsx
@@ -9,7 +9,10 @@ const reservasiDetailsaya
 
     const reservasiId = (await params).id
   return (
-    <div>reservasiDetailsaya
+    <div className='min-h-screen bg-slate-100'>
+        <div className='max-w-screen-lg mx-auto mt-10 py-20 px-4'>
+            {/* reservation detail */} 
+        </div>
         
     </div>
   )
