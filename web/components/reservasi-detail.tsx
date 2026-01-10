f4e5147cc16ef9a1cb4588286b411ed4ce799d18
Oka Windana Saputra <okwan2004@gmail.com>
2026-01-10 08:47:10 +0700

    update label reservasi ID di detail

--- web/components/reservasi-detail.tsx
+++ web/components/reservasi-detail.tsx
@@ -6,7 +6,11 @@ const ReservasiDetail = ({reseservasiId} : {reseservasiId: string}) => {
         <div className='grid md:grid-cols-2 md:gap-5'>
             <ul>
                 <li className='py-2'>
-                    
+                    <div className="flex items-center">
+                        <div className="flex-1 min-w-0 ms-4">
+                            <p className='text-sm font-medium text-gray-800 truncate'>Reservasi ID</p>
+                        </div>
+                    </div>
                 </li>
             </ul>
         </div>
