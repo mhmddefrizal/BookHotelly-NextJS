ca78c4e2c39e75dcdcf182dda9a8d52b6d065596
Rivaldzz <rivaldosborn@gmail.com>
2026-01-10 13:03:42 +0700

    import suspense ke dashboard page

--- web/app/admin/dashboard/page.tsx
+++ web/app/admin/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from 'next';
 import DashboardCards from '@/components/admin/dashboard-cards';
+import { Suspense } from 'react';
 
 // Metadata untuk halaman dashboard admin
 export const metadata: Metadata = {
@@ -11,7 +12,9 @@ const HalamanDashboard = () => {
   return (
     <div className='max-w-screen-xl px-4 py-16 mt-10 mx-auto'>
       <h1 className='text-4xl font-bold text-gray-500'>Dashboard</h1>
-      <DashboardCards />
+      <Suspense fallback={<p>Loading...</p>}>
+        <DashboardCards />
+      </Suspense>
     </div>
   )
 }
