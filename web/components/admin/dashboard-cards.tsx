048606bac0256f696b86e77fa969b40759d97f71
Rivaldzz <rivaldosborn@gmail.com>
2026-01-10 12:57:53 +0700

    buat card untuk menampilkan total pengguna

--- web/components/admin/dashboard-cards.tsx
+++ web/components/admin/dashboard-cards.tsx
@@ -26,7 +26,17 @@ const DashboardCards = () => {
                     <p className='text-3xl'>999</p>
                 </div>
             </div>
-            
+            {/* card untuk menampilkan total pengguna */}
+            <div className="flex items-center bg-white border rounded-md overflow-hidden shadow-sm">
+                <div className="p-4 bg-green-400">
+                    {/* content */}
+                    <LuUser className="text-white" size={12} />
+                </div>
+                <div className="px-4 text-gray-400">
+                    <h3 className='text-sm tracking-winder'>Total Pengguna</h3>
+                    <p className='text-3xl'>999</p>
+                </div>
+            </div>
         </div>
     )
 }
