e9714a2e01fdc5c4bd9d626ce51e0c4f267b68cd
Oka Windana Saputra <okwan2004@gmail.com>
2026-01-10 08:43:22 +0700

    buat layout detail reservasi

--- web/components/reservasi-detail.tsx
+++ web/components/reservasi-detail.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const ReservasiDetail = ({reseservasiId} : {reseservasiId: string}) => {
   return (
-    <div>ReservasiDetail</div>
+    <div className='w-full p-4 bg-white border border-gray-300 rounded-sm shadow'>
+        <div className='grid md:grid-cols-2 md:gap-5'>
+            <ul>
+                <li className='py-2'>
+                    
+                </li>
+            </ul>
+        </div>
+    </div>
   )
 }
 
