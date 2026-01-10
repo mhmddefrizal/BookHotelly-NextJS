fda6cca435fa37bc47ebb0774f9519fe2b97718b
Rivaldzz <rivaldosborn@gmail.com>
2026-01-10 13:00:41 +0700

    import card dashboard ke page dashboard

--- web/app/admin/dashboard/page.tsx
+++ web/app/admin/dashboard/page.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import DashboardCards from '@/components/admin/dashboard-cards'
 
 const HalamanDashboard = () => {
   return (
-    <div className='py-20 mt-10'>Halaman Dashboard</div>
+    <div className='max-w-screen-xl px-4 py-16 mt-10 mx-auto'>
+      <h1 className='text-4xl font-bold text-gray-500'>Dashboard</h1>
+      <DashboardCards />
+    </div>
   )
 }
 
