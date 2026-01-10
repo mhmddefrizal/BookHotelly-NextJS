26583112cd6d0f9f59aea9ed17f8bfd480cb5e4f
Oka Windana Saputra <okwan2004@gmail.com>
2026-01-10 10:08:16 +0700

    masukan render tadi ke dalam suspense

--- web/app/reservasisaya/[id]/page.tsx
+++ web/app/reservasisaya/[id]/page.tsx
@@ -18,7 +18,10 @@ const reservasiDetailsaya
     <div className='min-h-screen bg-slate-100'>
         <div className='max-w-screen-lg mx-auto mt-10 py-20 px-4'>
             {/* reservation detail */}
-            <ReservasiDetail reseservasiId={reservasiId} />
+            <Suspense>
+                <ReservasiDetail reseservasiId={reservasiId} />
+            </Suspense>
+            
         </div>
         
     </div>
