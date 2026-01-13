90168eb15be3ba032a68d5720b02ed20b770cacb
mhmddefrizal <muhammaddefrizal08@gmail.com>
2026-01-13 17:21:34 +0700

    perbaikan: merapikan format kode dan penataan ulang elemen dalam komponen PaymentButton

--- web/components/payment-button.tsx
+++ web/components/payment-button.tsx
@@ -1,49 +1,45 @@
-'use client';
+"use client";
 
-import { useTransition } from 'react';
-import { reservationProps } from '@/types/reservation';
+import { useTransition } from "react";
+import { reservationProps } from "@/types/reservation";
 
 // menambahkan tipe global untuk window.snap
 declare global {
-    interface Window {
-        snap: {
-            pay: (token: string) => void;
-        };
-    }
+  interface Window {
+    snap: {
+      pay: (token: string) => void;
+    };
+  }
 }
 
 // tombol pembayaran
-const PaymentButton = ({
-    reservation,
-}: {
-    reservation: reservationProps;
-}) => {
-    // handle pembayaran
-    const [isPending, startTransition] = useTransition();
-    const handlePayment = async () => {
-        // memulai proses pembayaran
-        startTransition(async () => {
-            try {
-                const response = await fetch('/api/payment', {
-                    method: 'POST',
-                    body : JSON.stringify(reservation),
-                });
-                const { token } = await response.json();
-                if(token) {
-                    // redirect ke halaman pembayaran Midtrans
-                    window.snap.pay(token);
-                }
-            } catch (error) {
-                console.error('gagal membayar', error);
-            }
+const PaymentButton = ({ reservation }: { reservation: reservationProps }) => {
+  // handle pembayaran
+  const [isPending, startTransition] = useTransition();
+  const handlePayment = async () => {
+    // memulai proses pembayaran
+    startTransition(async () => {
+      try {
+        const response = await fetch("/api/payment", {
+          method: "POST",
+          body: JSON.stringify(reservation),
         });
-    }
+        const { token } = await response.json();
+        if (token) {
+          // redirect ke halaman pembayaran Midtrans
+          window.snap.pay(token);
+        }
+      } catch (error) {
+        console.error("gagal membayar", error);
+      }
+    });
+  };
 
   return (
     <button onClick={handlePayment} className="px-10 py-4 mt-2 text-center font-semibold bg-blue-600 text-white w-full rounded-sm hover:bg-blue-700 cursor-pointer">
-        Bayar Sekarang
+      {isPending ? "Processing..." : "Process Payment"}
     </button>
-  )
-}
+  );
+};
 
-export default PaymentButton
+export default PaymentButton;
