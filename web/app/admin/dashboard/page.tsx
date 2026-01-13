a4627afcde9ec2ea588e11fe46f72cc273821e0b
mhmddefrizal <muhammaddefrizal08@gmail.com>
2026-01-13 17:14:57 +0700

    perbaikan: merapikan format kode dan penataan ulang elemen dalam halaman dashboard

--- web/app/admin/dashboard/page.tsx
+++ web/app/admin/dashboard/page.tsx
@@ -1,28 +1,28 @@
-import { Metadata } from 'next';
-import DashboardCards from '@/components/admin/dashboard-cards';
-import ReservationList from '@/components/admin/reservation-list';
-import { Suspense } from 'react';
+import { Metadata } from "next";
+import DashboardCards from "@/components/admin/dashboard-cards";
+import ReservationList from "@/components/admin/reservation-list";
+import { Suspense } from "react";
 
 // Metadata untuk halaman dashboard admin
 export const metadata: Metadata = {
-  title: 'Dashboard Admin',
-  description: 'Halaman dashboard untuk admin',
+  title: "Dashboard Admin",
+  description: "Halaman dashboard untuk admin",
 };
 
 const HalamanDashboard = () => {
   return (
-    <div className='max-w-screen-xl px-4 py-16 mt-10 mx-auto'>
-      <h1 className='text-4xl font-bold text-gray-500'>Dashboard</h1>
+    <div className="max-w-screen-xl px-4 py-16 mt-10 mx-auto">
+      <h1 className="text-4xl font-bold text-gray-500">Dashboard</h1>
       {/* tampilkan card dan daftar reservasi */}
-      <Suspense fallback={<p>Loading...</p>}>
+      <Suspense fallback={<p>Loading Cards...</p>}>
         <DashboardCards />
       </Suspense>
       {/* tampilkan daftar reservasi */}
-      <Suspense fallback={<p>Loading...</p>}>
+      <Suspense fallback={<p>Loading Reservation...</p>}>
         <ReservationList />
       </Suspense>
     </div>
-  )
-}
+  );
+};
 
-export default HalamanDashboard
+export default HalamanDashboard;
