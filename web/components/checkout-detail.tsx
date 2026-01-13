c89894320bc819c1ed03f0350621ce01c82624bb
mhmddefrizal <muhammaddefrizal08@gmail.com>
2026-01-13 17:45:40 +0700

    perbaikan: merapikan format kode dan penataan ulang elemen dalam komponen CheckoutDetail

--- web/components/checkout-detail.tsx
+++ web/components/checkout-detail.tsx
@@ -1,109 +1,102 @@
-import Image from 'next/image'
-import { getReservationById } from '@/lib/data';
-import { formatDate, formatCurrency } from '@/lib/utils';
-import { differenceInCalendarDays } from 'date-fns';
-import PaymentButton from '@/components/payment-button';
-
+import Image from "next/image";
+import { getReservationById } from "@/lib/data";
+import { formatDate, formatCurrency } from "@/lib/utils";
+import { differenceInCalendarDays } from "date-fns";
+import PaymentButton from "@/components/payment-button";
 
 // import fungsi untuk mengambil data reservasi berdasarkan ID
 const CheckoutDetail = async ({ reservationId }: { reservationId: string }) => {
-    const reservation = await getReservationById(reservationId);
-    // jika data reservasi tidak ditemukan, tampilkan pesan
-    if (!reservation || !reservation.Payment) return <h1>Tidak Ditemukan Data</h1>
+  const reservation = await getReservationById(reservationId);
+  // jika data reservasi tidak ditemukan, tampilkan pesan
+  if (!reservation || !reservation.Payment) return <h1>Tidak Ditemukan Data</h1>;
 
-    // hitung durasi menginap
-    const duration = differenceInCalendarDays(reservation.endDate, reservation.startDate);
+  // hitung durasi menginap
+  const duration = differenceInCalendarDays(reservation.endDate, reservation.startDate);
 
-    return (
-        <div className='grid md:grid-cols-2 gap-5'>
-            <div className="order-2">
-                <div className="flex flex-col mb-3 items-start bg-white border border-gray-200 rounded-sm md:flex-row md:w-full">
-                    {/* menampilkan gambar kamar */}
-                    <div className="aspect-video relative w-full md:w-1/2">
-                        <Image src={reservation.Room.image} alt={reservation.Room.name} fill className="object-cover rounded-t-sm md:rounded-t-none md:rounded-s-sm" />
-                    </div>
-                    {/* menampilkan detail kamar */}
-                    <div className="flex flex-col justify-between p-4 leading-normal w-full">
-                        <h5 className='mb-1 text-4xl font-bold tracking-tight text-gray-600'>{reservation.Room.name}</h5>
-                        <div className="flex items-center gap-1 text-2xl text-gray-700">
-                            <div className="flex items-center justify-center gap-1">
-                                {/* menampilkan harga */}
-                                <span className='text-2xl'>
-                                    {formatCurrency(reservation.price)}
-                                </span>
-                                <span>/ malam</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                {/* tombol bayar */}
-                <PaymentButton reservation={reservation} />
+  return (
+    <div className="grid md:grid-cols-2 gap-5">
+      <div className="order-2">
+        <div className="flex flex-col mb-3 items-start bg-white border border-gray-200 rounded-sm md:flex-row md:w-full">
+          {/* menampilkan gambar kamar */}
+          <div className="aspect-video relative w-full md:w-1/2">
+            <Image src={reservation.Room.image} alt={reservation.Room.name} fill className="object-cover rounded-t-sm md:rounded-t-none md:rounded-s-sm" />
+          </div>
+          {/* menampilkan detail kamar */}
+          <div className="flex flex-col justify-between p-4 leading-normal w-full">
+            <h5 className="mb-1 text-4xl font-bold tracking-tight text-gray-600">{reservation.Room.name}</h5>
+            <div className="flex items-center gap-1 text-2xl text-gray-700">
+              <div className="flex items-center justify-center gap-1">
+                {/* menampilkan harga */}
+                <span className="text-2xl">{formatCurrency(reservation.price)}</span>
+                <span>/ malam</span>
+              </div>
             </div>
-            <div className="border border-gray-300 px-3 py-5 rounded-md p-5 bg-slate-50">
-                <table className='w-full'>
-                    <tbody>
-                        {/* ID reservasi */}
-                        <tr>
-                            <td className='py-2'>ID Reservasi</td>
-                            <td className='py-2 text-right truncate'>{reservation.id}</td>
-                        </tr>
-                        {/* nama */}
-                        <tr>
-                            <td className='py-2'>Nama</td>
-                            <td className='py-2 text-right truncate'>{reservation.User.name}</td>
-                        </tr>
-                        {/* email */}
-                        <tr>
-                            <td className='py-2'>Email</td>
-                            <td className='py-2 text-right truncate'>{reservation.User.email}</td>
-                        </tr>
-                        {/* nomor telepon */}
-                        <tr>
-                            <td className='py-2'>Nomor Telepon</td>
-                            <td className='py-2 text-right truncate'>{reservation.User.phone}</td>
-                        </tr>
-                        {/* tanggal kedatangan */}
-                        <tr>
-                            <td className='py-2'>Kedatangan</td>
-                            <td className='py-2 text-right truncate'>{formatDate(reservation.startDate.toISOString())}</td>
-                        </tr>
-                        {/* tanggal kepergian */}
-                        <tr>
-                            <td className='py-2'>Kepergian</td>
-                            <td className='py-2 text-right truncate'>{formatDate(reservation.endDate.toISOString())}</td>
-                        </tr>
-                        <tr>
-                            <td className='py-2'>Durasi</td>
-                            <td className='py-2 text-right truncate'>
-                                {/* menampilkan durasi menginap */}
-                                <span>
-                                    {duration} {duration === 1 ? "malam" : "malam"}
-                                </span>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className='py-2'>Harga Total</td>
-                            <td className='py-2 text-right truncate'>
-                                {/* menampilkan harga total */}
-                                <span>
-                                    {formatCurrency(reservation.Payment.amount)}
-                                </span>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className='py-2'>Status Pembayaran</td>
-                            <td className='py-2 text-right truncate'>
-                                {/* menampilkan status pembayaran */}
-                                <span>
-                                    {reservation.Payment.status}
-                                </span>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+          </div>
         </div>
-    )
-}
+        {/* tombol bayar */}
+        <PaymentButton reservation={reservation} />
+      </div>
+      <div className="border border-gray-300 px-3 py-5 rounded-md p-5 bg-slate-50">
+        <table className="w-full">
+          <tbody>
+            {/* ID reservasi */}
+            <tr>
+              <td className="py-2">ID Reservasi</td>
+              <td className="py-2 text-right truncate">{reservation.id}</td>
+            </tr>
+            {/* nama */}
+            <tr>
+              <td className="py-2">Nama</td>
+              <td className="py-2 text-right truncate">{reservation.User.name}</td>
+            </tr>
+            {/* email */}
+            <tr>
+              <td className="py-2">Email</td>
+              <td className="py-2 text-right truncate">{reservation.User.email}</td>
+            </tr>
+            {/* nomor telepon */}
+            <tr>
+              <td className="py-2">Nomor Telepon</td>
+              <td className="py-2 text-right truncate">{reservation.User.phone}</td>
+            </tr>
+            {/* tanggal kedatangan */}
+            <tr>
+              <td className="py-2">Kedatangan</td>
+              <td className="py-2 text-right truncate">{formatDate(reservation.startDate.toISOString())}</td>
+            </tr>
+            {/* tanggal kepergian */}
+            <tr>
+              <td className="py-2">Kepergian</td>
+              <td className="py-2 text-right truncate">{formatDate(reservation.endDate.toISOString())}</td>
+            </tr>
+            <tr>
+              <td className="py-2">Durasi</td>
+              <td className="py-2 text-right truncate">
+                {/* menampilkan durasi menginap */}
+                <span>
+                  {duration} {duration === 1 ? "malam" : "malam"}
+                </span>
+              </td>
+            </tr>
+            <tr>
+              <td className="py-2">Harga Total</td>
+              <td className="py-2 text-right truncate">
+                {/* menampilkan harga total */}
+                <span>{formatCurrency(reservation.Payment.amount)}</span>
+              </td>
+            </tr>
+            <tr>
+              <td className="py-2">Status Pembayaran</td>
+              <td className="py-2 text-right truncate">
+                {/* menampilkan status pembayaran */}
+                <span>{reservation.Payment.status}</span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
 
-export default CheckoutDetail
+export default CheckoutDetail;
